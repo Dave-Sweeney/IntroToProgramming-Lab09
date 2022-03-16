f5b48068d6d22248305a80fd33105359fcbd325a
David J. Sweeney <david.j.sweeney@outlook.com>
2022-03-16 14:07:11 -0400

    Updated to include file stream.

--- IntroToProgramming-Lab09/Program.cs
+++ IntroToProgramming-Lab09/Program.cs
@@ -10,7 +10,18 @@ namespace IntroToProgramming_Lab09
     {
         static void Main(string[] args)
         {
+            // EX 1 Task 1.2:  Write output to the Console window to prompt the user for deciding to create
+            // a student class or an instructor class.
+            Console.WriteLine("Do you want to create a:\n S - Student\n I - Instructor\n Q - Quit");
+            
+            // EX 1 Task 2:  Read the response from the user by using the Console.Read() method.
+            // Store the value in a char variable.
 
+            char input = (char)Console.Read();
+
+            Console.WriteLine("You said: {0}", input);
+
+            Console.ReadKey();
         }
     }
 }
